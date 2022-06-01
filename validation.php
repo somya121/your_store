1b236cec9a23a52927922b139359bbb40755154b
SOMYA SINGH <67359438+somya121@users.noreply.github.com>
2022-06-01 17:31:57 +0530

    Update validation.php

--- validation.php
+++ validation.php
@@ -10,7 +10,7 @@ $result = mysqli_query($con, $s);
 $num = mysqli_num_rows($result);
 if($num==1){
     $_SESSION['username']= $name;
-header('location:index.php');
+header('location:home.php');
 }
 else{
  header('location:account.php') ;
